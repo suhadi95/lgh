cbf1ecc85cfe22f5b034e0866a6c80b6771212a4
suhadi95 <suhadiparman@gmail.com>
2025-10-31 13:50:07 +0700

    Revert "Add dashboard controller and view"
    This reverts commit 80f5d26a4cb90509050bd14da05e0b629bfbcf71.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('home');
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
